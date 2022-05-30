dbaa799ecea6b23701a4263cc711cffecb853d88
EbiPenMan <karimi.gamedev@gmail.com>
2022-05-30 20:44:34 +0430

    before truck world modules

--- src/module/login/hook/hook_login.ts
+++ src/module/login/hook/hook_login.ts
@@ -16,24 +16,34 @@ const hook_login_beforeAuthenticateCustom: nkruntime.BeforeHookFunction<nkruntim
     function (ctx: nkruntime.Context, logger: nkruntime.Logger, nk: nkruntime.Nakama, request: nkruntime.AuthenticateCustomRequest) {
         
         try {
+            logger.info("hook_login_beforeAuthenticateCustom start");
             if (request.account.vars.token == null || request.account.vars.token == "") {
                 return null;
             }
             
+            logger.info("hook_login_beforeAuthenticateCustom 1");
+    
             let tokenStr: string = decrypt(getPrivateLoginConfigs(nk, logger)[1].tokenSeed, request.account.vars.token);
             if (tokenStr == null || tokenStr == "") {
                 return null;
             }
-            
+            logger.info("hook_login_beforeAuthenticateCustom 2");
+    
             let tokenParsed: any = JSON.parse(tokenStr);
-            
-            
+    
+            logger.info("hook_login_beforeAuthenticateCustom 3");
+            logger.info("hook_login_beforeAuthenticateCustom tokenParsed: " + JSON.stringify(tokenParsed));
+            logger.info("parseISOString(tokenParsed.createdAt).getTime(): " + new Date(tokenParsed.createdAt).getTime());
+            logger.info("dateAdd(new Date(), 'month', 6): " + dateAdd(new Date(), 'month', 6).getTime());
+    
             if (tokenParsed.phoneNumber == null || tokenParsed.phoneNumber == "" ||
                 tokenParsed.unique_identifier == null || tokenParsed.unique_identifier == "" ||
                 tokenParsed.createdAt == null ||
-                parseISOString(tokenParsed.createdAt).getTime() > dateAdd(new Date(), 'month', 6).getTime()) {
+                new Date(tokenParsed.createdAt).getTime() > dateAdd(new Date(), 'month', 6).getTime()) {
+                logger.info("hook_login_beforeAuthenticateCustom 4");
                 return null;
             }
+            logger.info("hook_login_beforeAuthenticateCustom 5");
     
             request.account.id = tokenParsed.phoneNumber;
             
